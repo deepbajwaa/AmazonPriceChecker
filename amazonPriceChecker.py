044f74e4f9e5dc2736a161cb16b1e9301113037f
deep.bajwaa <deep43055@gmail.com>
2019-12-29 12:37:21 -0500

    Set up the project

---
+++ amazonPriceChecker.py
@@ -0,0 +1,5 @@
+import requests
+
+URL = "https://www.amazon.ca/Samsung-RU7100-UN75RU7100FXZC-Canada-Version/dp/B07NC7N3G6/ref=sr_1_3?keywords=samsung+tv&qid=1577636170&refinements=p_n_size_browse-bin%3A17304154011&rnid=3236496011&s=electronics&sr=1-3"
+
+headers = {}

359cd20f1e07ae0294b01762f5bf1e1c08921a27
deep.bajwaa <deep43055@gmail.com>
2019-12-31 13:33:42 -0500

    I was now able to get the name of the product from amazon that I wanted to monitor

--- amazonPriceChecker.py
+++ amazonPriceChecker.py
@@ -1,5 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://www.amazon.ca/Samsung-RU7100-UN75RU7100FXZC-Canada-Version/dp/B07NC7N3G6/ref=sr_1_3?keywords=samsung+tv&qid=1577636170&refinements=p_n_size_browse-bin%3A17304154011&rnid=3236496011&s=electronics&sr=1-3"
 
-headers = {}
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
+
+page = requests.get(URL, headers=headers) #This is used to request the URL from the server
+
+soup1 = BeautifulSoup(page.content, "html.parser")
+
+soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+title = soup2.find(id= "productTitle") #Used to parse the html page
+
+print(title)
